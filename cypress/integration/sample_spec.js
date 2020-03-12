f3dd0e70484ab38c4b9e87e6a4a0a3208d68cf35
Daniel Fuentes <daniel.fuentes@turner.com>
2020-03-12 12:26:42 -0500

    percitest

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -16,7 +16,7 @@ describe('My First Test', () => {
 describe('Integration test with visual testing', function() {
   it('Loads the homepage', function() {
     // Load the page or perform any other interactions with the app.
-    cy.visit('https://example.cypress.io')
+    cy.visit('https://gorillalogic.com/')
     // Take a snapshot for visual diffing
     cy.percySnapshot();
   });
